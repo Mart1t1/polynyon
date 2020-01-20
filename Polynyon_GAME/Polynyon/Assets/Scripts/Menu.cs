09d7745499ea1d54e1b3085446c09b3bc2a786fe
Hugo <hugo.mussard@epita.fr>
2020-01-20 13:01:52 +0100

    menu1

---
+++ Polynyon_GAME/Polynyon/Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject Menuu;
+    private bool Checkk = false;
+
+    public void PlayGame()
+    {
+        
+        SceneManager.LoadScene(1);
+        
+    }
+
+  
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
