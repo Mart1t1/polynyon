692bf9c148724a6312c5d2c8ac44cea30e548de3
Mayes TIDJEDAM <mayes.tidjedam@epita.fr>
2020-01-27 17:47:06 +0100

    première salle toujours

---
+++ Polynyon_GAME/Polynyon/Assets/moves.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moves : MonoBehaviour
+{
+    public Rigidbody perso;
+
+    public int déplacement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("q"))
+        {
+            transform.Translate(new Vector3(-50f*Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(new Vector3(50f*Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey("z"))
+        {
+            transform.Translate(new Vector3(0, 0, 50f*Time.deltaTime));
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(0, 0, -50f*Time.deltaTime));
+        }
+    }
+}
